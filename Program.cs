e27cccbad9be4bd72c2bde9374a59a070d995de9
Bottle_Baron <petrus.h.johansson@gmail.com>
2022-11-08 11:26:59 +0100

    Createbooking functional

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿namespace BlodBanken_Fabian_Henrik_Petrus;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        // For testing
+        DataManager dataManager = new();
+        dataManager.CreateBooking("1, 1, false, NULL");
     }
 }
