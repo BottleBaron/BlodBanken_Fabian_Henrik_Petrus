3c97c9f360f24b334744cf99cd4786fb8d872288
Bottle_Baron <petrus.h.johansson@gmail.com>
2022-11-14 10:55:30 +0100

    Sketch

---
+++ GUI/StaffGUI.cs
@@ -0,0 +1,13 @@
+
+
+// Main menu
+// 1. Visa
+// 2. Välj -> 
+//                                Event handler
+//        UI                UI         v     Mngr          Crud
+// 3. Skapa Profil -> ProfilSkaparMeny -> Validator -> Create Donor
+                      // Välj Namn:
+                      // Adress:
+                      // etc.
+
+
