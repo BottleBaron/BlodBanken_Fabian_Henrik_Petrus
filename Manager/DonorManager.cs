0a462a022bcd4f7254c502d78ec60e8a2cc426d6
Bottle_Baron <petrus.h.johansson@gmail.com>
2022-11-16 10:33:50 +0100

    Fixed some validation

--- Manager/DonorManager.cs
+++ Manager/DonorManager.cs
@@ -3,19 +3,19 @@ namespace BlodBanken_Fabian_Henrik_Petrus;
 internal class DonorManager
 {
     private DonorDB _donorDb = new();
-    
+
     // Change this to appeal to crud structure 
-     //public List<Donor> GetDonors(int bloodType)
-    
-        //List<Donor> foundDonors = SQLWriter.sp_SelectTable<Donor>("*", $"donors WHERE blood_type = {bloodType}");
-        //return foundDonors;
-    
+    //public List<Donor> GetDonors(int bloodType)
+
+    //List<Donor> foundDonors = SQLWriter.sp_SelectTable<Donor>("*", $"donors WHERE blood_type = {bloodType}");
+    //return foundDonors;
+
 
     public bool ValidateName(string name)
     {
         //Checks that the name contains a blankspace and is more than 5 characters long. 
         //Expand method to check for upper case etc. 
-        if (name.Contains(' ')&& name.Count()>5)
+        if (name.Contains(' ') && name.Count() > 5)
         {
             return true;
         }
@@ -27,41 +27,27 @@ internal class DonorManager
     }
 
 
-    public bool ValidateAddress(string [] address)
+    public bool ValidateAddress(string[] address)
     {
-        
         //Checks that the fields are not empty. 
         if (address[0].Length > 0 && address[1].Length > 0 && address[2].Length > 0)
-        {   
+        {
             //Checks that the zip code is 5 digits long. 
             if (address[1].Length == 5)
             {
                 return true;
             }
-
-            else
-            {
-                return false;
-            }
-            
         }
-
-        else
-        {
-            return false;
-        }
+        return false;
     }
 
 
-
-
-     public bool ValidatePhoneNumber(string phoneNumber)
+    public bool ValidatePhoneNumber(string phoneNumber)
     {
-        
         //Checks that the phonenumber only contains digits and more than 8 digits. 
-        if (phoneNumber.All(Char.IsDigit)==true && phoneNumber.Length>8)
-        {   
-           return true;      
+        if (phoneNumber.All(Char.IsDigit) == true && phoneNumber.Length > 8)
+        {
+            return true;
         }
 
         else
@@ -72,26 +58,10 @@ internal class DonorManager
 
 
     public bool ValidateDateOfBirth(string dateOfBirth)
-    {   
-            try
-            {
-            //Converts string to ints
-            int year, month, day;
-            year = Convert.ToInt32(dateOfBirth.Substring(0, 4));
-            month = Convert.ToInt32(dateOfBirth.Substring(4, 2));
-            day = Convert.ToInt32(dateOfBirth.Substring(6, 2));
-
-            //Assign ints to a Dateonly. If success the date is correct. 
-            DateOnly dateOfBirthDO = new(year,month,day);
-            return true;
-          
-
-            }
-            catch (System.Exception)
-            {
-                
-                return false;
-            }                   
+    {
+        if (DateTime.TryParse(dateOfBirth, out DateTime result)) return true;
+            
+        return false;
     }
 
 
@@ -100,32 +70,23 @@ internal class DonorManager
         try
         {
             int bloodType = Convert.ToInt32(keyPress.ToString());
-          if (bloodType >=1 && bloodType <=8)
-        {
-            return true;
-        }
-        else return false;
+            if (bloodType >= 1 && bloodType <= 8)
+            {
+                return true;
+            }
+            else return false;
         }
         catch (System.Exception)
         {
-            
-            
             return false;
         }
     }
 
-    
+
     public int SaveDonorToDB(Donor newDonor)
     {
         int savedDonorId;
         savedDonorId = _donorDb.Create(newDonor);
         return savedDonorId;
-
     }
-
-
-
-
-
-
 }
